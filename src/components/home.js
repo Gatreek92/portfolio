84feba772974599c579dd928cae781957fb0a049
hamdi <hamdigatri92@gmail.com>
2019-04-20 22:29:58 +0100

    Create needed components.

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from 
+const Home = () => (
+    <div>
+        <Header/>
+        <Menu/>
+        <ContentContainer/>
+        <Illustration/>
+        <Footer/>
+    </div>
+)
