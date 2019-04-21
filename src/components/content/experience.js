6a08f4df14de17ccf371d9fc73f6d618069b7ede
hamdi <hamdigatri92@gmail.com>
2019-04-21 17:36:54 +0100

    Add experience content.

---
+++ src/components/content/experience.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Experience = () => (
+  <div>
+    <ul>
+      <li>
+        Full-stack javascript developer, at xTech development services since Nov
+        2017:
+        <ul>
+          <li>
+            Built web and mobile applications using ReactJs,React Native, VueJs
+            and NodeJs
+          </li>
+          <li>Created web landing pages using Bootstrap.</li>
+          <li>Tested front-end code.</li>
+        </ul>
+      </li>
+      <li>
+        Web/App developer, at Tree (training company) from septembre to november
+        2017 :
+        <ul>
+          <li>Developed HR application with Java.</li>
+          <li>Managed the company website using wordpress</li>
+        </ul>
+      </li>
+      <li>
+        Web developer, at EDT from set 2015 to dec 2015 :
+        <ul>
+          <li>Created static web pages using HTML,CSS,JS</li>
+          <li>Created web animations using CSS,and JQUERY</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Experience;
