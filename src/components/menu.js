8d13a6e4fa87b695b05fd06c7619affbfb7f6c29
hamdi <hamdigatri92@gmail.com>
2019-04-21 22:34:21 +0100

    Styling menu.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 const Menu = () => (
-  <ul>
-    <li>
-      <Link to="/about">Level 1 : About me</Link>
-    </li>
-    <li>
-      <Link to="/experience">Level 2 : Experience</Link>
-    </li>
-    <li>
-      <Link to="/skills">Level 3 : Skills</Link>
-    </li>
-    <li>
-      <Link to="/projects">Level 4 : Projects</Link>
-    </li>
-    <li>
-      <Link to="/education">Level 5 : Education</Link>
-    </li>
-    <li>
-      <Link to="/contact">Level 6 : Contact</Link>
-    </li>
-  </ul>
+  <div className="menu-container">
+    <ul>
+      <li>
+        <Link to="/about">Level 1 : About me</Link>
+      </li>
+      <li>
+        <Link to="/experience">Level 2 : Experience</Link>
+      </li>
+      <li>
+        <Link to="/skills">Level 3 : Skills</Link>
+      </li>
+      <li>
+        <Link to="/projects">Level 4 : Projects</Link>
+      </li>
+      <li>
+        <Link to="/education">Level 5 : Education</Link>
+      </li>
+      <li>
+        <Link to="/contact">Level 6 : Contact</Link>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Menu;
