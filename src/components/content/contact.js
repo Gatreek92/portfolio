cc4ef349db29fb9a96df9a121e3e4547128cc863
hamdi <hamdigatri92@gmail.com>
2019-04-21 18:01:41 +0100

    Add contact content.

---
+++ src/components/content/contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+const Contact = () => (
+  <div>
+    <p> contact me via Email : hamdigatri92@gmail.com </p>
+    <p>Phone number : +21693593182</p>
+    <p>Find me on social media</p>
+    <ul>
+      <li>
+        <a href="https://bit.ly/2R1mcwZ">Linkedin</a>
+      </li>
+      <li>
+        <a href="https://bit.ly/2s43KoG">Github</a>
+      </li>
+      <li>
+        <a href="https://bit.ly/2F4bi2e"> Twitter </a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Contact;
