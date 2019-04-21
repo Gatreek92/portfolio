306f587be8dd329e330f3c8d02adc9965dfd5475
hamdi <hamdigatri92@gmail.com>
2019-04-21 20:44:08 +0100

    Delete router from Menu component.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,30 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 const Menu = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/about">Level 1 : About me</Link>
-        </li>
-        <li>
-          <Link to="/experience">Level 2 : Experience</Link>
-        </li>
-        <li>
-          <Link to="/skills">Level 3 : Skills</Link>
-        </li>
-        <li>
-          <Link to="/projects">Level 4 : Projects</Link>
-        </li>
-        <li>
-          <Link to="/education">Level 5 : Education</Link>
-        </li>
-        <li>
-          <Link to="/contact">Level 6 : Contact</Link>
-        </li>
-      </ul>
-    </div>
-  </Router>
+  <ul>
+    <li>
+      <Link to="/about">Level 1 : About me</Link>
+    </li>
+    <li>
+      <Link to="/experience">Level 2 : Experience</Link>
+    </li>
+    <li>
+      <Link to="/skills">Level 3 : Skills</Link>
+    </li>
+    <li>
+      <Link to="/projects">Level 4 : Projects</Link>
+    </li>
+    <li>
+      <Link to="/education">Level 5 : Education</Link>
+    </li>
+    <li>
+      <Link to="/contact">Level 6 : Contact</Link>
+    </li>
+  </ul>
 );
 
 export default Menu;
