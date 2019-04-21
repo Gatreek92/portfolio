554ca847ee93823baceaa1cda9169debd982302d
hamdi <hamdigatri92@gmail.com>
2019-04-21 18:01:55 +0100

    Import and render all content.

--- src/components/contentContainer.js
+++ src/components/contentContainer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import About from "./content/about";
+import Experience from "./content/experience";
+import Skills from "./content/skills";
+import Projects from "./content/projects";
+import Education from "./content/education";
+import Contact from "./content/contact";
+
 const ContentContainer = () => (
   <div className="content-container">
-    <p>content goes here !</p>
+    <About />
+    <Experience />
+    <Skills />
+    <Projects />
+    <Education />
+    <Contact />
   </div>
 );
 
