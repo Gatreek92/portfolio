15902ac3efcfdf77189bcb6a6e632722c832f001
hamdi <hamdigatri92@gmail.com>
2019-04-21 09:16:18 +0100

    Add menu content.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,14 @@
 import React from "react";
 const Menu = () => (
   <div>
-    <p>Menu goes here !</p>
+    <ul>
+      <li>Level 1 : About me</li>
+      <li>Level 2 : Experience</li>
+      <li>Level 3 : Skills</li>
+      <li>Level 4 : Projects</li>
+      <li>Level 5 : Education</li>
+      <li>Level 6 : Contact</li>
+    </ul>
   </div>
 );
 
