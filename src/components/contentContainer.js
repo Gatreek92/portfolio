e14260aa1ddd192d34c7672edd31cfecd828d5ac
hamdi <hamdigatri92@gmail.com>
2019-04-21 20:44:39 +0100

    Create routes.

--- src/components/contentContainer.js
+++ src/components/contentContainer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import About from "./content/about";
 import Experience from "./content/experience";
 import Skills from "./content/skills";
@@ -8,12 +10,12 @@ import Contact from "./content/contact";
 
 const ContentContainer = () => (
   <div className="content-container">
-    <About />
-    <Experience />
-    <Skills />
-    <Projects />
-    <Education />
-    <Contact />
+    <Route path="/about" component={About} />
+    <Route path="/experience" component={Experience} />
+    <Route path="/skills" component={Skills} />
+    <Route path="/projects" component={Projects} />
+    <Route path="/education" component={Education} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
