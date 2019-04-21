5cfa9bdca9d2d3b5dd030bebe414b6f91af151dd
hamdi <hamdigatri92@gmail.com>
2019-04-21 12:14:15 +0100

    Add footer content.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,18 @@
 import React from "react";
 const Footer = () => (
   <footer>
-    <p>Page footer</p>
+    <p>Find me on social media</p>
+    <ul>
+      <li>
+        <a href="https://bit.ly/2R1mcwZ">Linkedin</a>
+      </li>
+      <li>
+        <a href="https://bit.ly/2s43KoG">Github</a>
+      </li>
+      <li>
+        <a href="https://bit.ly/2F4bi2e"> Twitter </a>
+      </li>
+    </ul>
   </footer>
 );
 
